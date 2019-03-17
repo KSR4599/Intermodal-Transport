5de78056382ea400610f7380887f87b894e065af
KSR <ksreddy9945@gmail.com>
2019-03-17 22:39:55 +0530

    ##

--- ipfs.js
+++
@@ -1,8 +0,0 @@
-var ipfsClient = require('ipfs-http-client')
-
-var ipfs = ipfsClient('ipfs.infura.io', '5001', { protocol: 'https' })
-//var ipfs = ipfsClient('127.0.0.1', '5001', { protocol: 'http' })
-
-
-export default ipfs;
-
